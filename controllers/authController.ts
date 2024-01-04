dbdae41e8483e6ef22c31e8a64d9192122987ba4
sandeep3468 <s.sandeep2197@gmail.com>
2024-01-04 12:07:50 +0530

    Sign Up & Sign In APIs

--- controllers/authController.ts
+++ controllers/authController.ts
@@ -16,7 +16,9 @@ const signup = asyncHandler(
       res.status(400);
       throw new Error("User already exists");
     }
+
     const newUser = await User.create(req.body);
+    newUser.generateAuthToken();
     res.status(201).json({
       statusCode: 201,
       message: "User created successfully",
@@ -27,16 +29,32 @@ const signup = asyncHandler(
 
 const signin = asyncHandler(
   async (req: Request, res: Response, next: NextFunction) => {
-    console.log("Signin route");
     if (!req.body.email || !req.body.password) {
       res.status(400);
       throw new Error("Email and password are required");
     }
     const user = await User.findOne({
       email: req.body.email,
-      password: req.body.password,
     });
-    const token = user?.generateAuthToken();
+
+    if (!user) {
+      res.status(400);
+      throw new Error("User not found");
+    }
+
+    const isMatch = await user.comparePassword(req.body.password);
+    if (!isMatch) {
+      res.status(400);
+      throw new Error("Invalid password");
+    }
+
+    const accessToken = user.generateAuthToken();
+    const refreshToken = user.generateAuthToken("30d");
+    res.status(200).json({
+      statusCode: 200,
+      message: "User signed in successfully",
+      data: { user, accessToken, refreshToken },
+    });
   }
 );
 
--- db/mongo/models/user.ts
+++ db/mongo/models/user.ts
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";
 import jwt from "jsonwebtoken";
+import { createHash } from "crypto";
 import dotenv from "dotenv";
 dotenv.config({ path: "./.env" });
 
@@ -9,6 +10,7 @@ interface IUser {
   firstName?: string;
   lastName?: string;
   role?: string;
+  passwordHash?: string;
 }
 
 const userSchema = new mongoose.Schema(
@@ -39,24 +41,62 @@ const userSchema = new mongoose.Schema(
       enum: ["user", "admin"],
       default: "user",
     },
+
+    passwordHash: {
+      type: String,
+      required: false,
+    },
+
+    accessToken: {
+      type: String,
+      required: false,
+    },
+
+    refreshToken: {
+      type: String,
+      required: false,
+    },
+
+    isVerified: {
+      type: Boolean,
+      default: false,
+    },
   },
-  { timestamps: true }
+  {
+    timestamps: true,
+    methods: {
+      generateAuthToken: function (expiresIn = "15m") {
+        const token = jwt.sign(
+          {
+            _id: this._id,
+            role: this.role,
+            email: this.email,
+            firstName: this.firstName,
+            lastName: this.lastName,
+          },
+          process.env.JWT_SECRET!,
+          { expiresIn: expiresIn }
+        );
+        return token;
+      },
+
+      comparePassword: async function (candidatePassword: string) {
+        const hash = createHash("sha256")
+          .update(candidatePassword)
+          .digest("hex");
+        return hash === this.passwordHash;
+      },
+    },
+  }
 );
 
-userSchema.methods.generateAuthToken = async function () {
-  const token = jwt.sign(
-    {
-      _id: this._id,
-      role: this.role,
-      email: this.email,
-      firstName: this.firstName,
-      lastName: this.lastName,
-    },
-    process.env.JWT_SECRET!,
-    { expiresIn: "1d" }
-  );
-  return token;
-};
+userSchema.pre("save", async function (next) {
+  console.log("pre save");
+  const user = this as IUser;
+  const hash = createHash("sha256").update(user.password).digest("hex");
+  user.passwordHash = hash;
+  next();
+});
 
 const User = mongoose.model("User", userSchema);
 
